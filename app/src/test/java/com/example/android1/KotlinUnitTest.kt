74f89e973a0f0685d9d3cb34fd0941605d93f1ab
Lannister <>
2022-12-06 21:07:25 +0900

    12.06

---
+++ app/src/test/java/com/example/android1/KotlinUnitTest.kt
@@ -0,0 +1,14 @@
+package com.example.android1
+
+import junit.framework.Assert.assertEquals
+import org.junit.Assert
+import org.junit.Test
+
+class KotlinUnitTest {
+
+    @Test
+    fun test() {
+
+        assertEquals(1,1)
+    }
+}
